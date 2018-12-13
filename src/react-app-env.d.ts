5ae258ac91f00b7a03cf2b7414d781022ecd5210
AppShore <git@appshore.com>
2018-12-13 19:08:23 +0100

    Add TS interfaces

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="react-scripts" />
+
+interface AppState {
+  latlng: L.LatLngExpression
+}
+
+interface UmapProps {
+  classes: WithStyles
+  latlng: L.LatLngExpression
+  zoom: number
+}
+
+interface UmapState {
+  latlng: L.LatLngExpression
+  zoom: number
+}
